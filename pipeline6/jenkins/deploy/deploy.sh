756ea82cacaf384d413c038d6de7cd0b1c851919
songbao2903 <93124426+songbao2903@users.noreply.github.com>
2022-03-16 16:27:02 +0700

    Update deploy.sh

--- pipeline6/jenkins/deploy/deploy.sh
+++ pipeline6/jenkins/deploy/deploy.sh
@@ -5,6 +5,6 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 #scp  /tmp/.auth root@192.168.100.10:/tmp/.auth
-scp  /tmp/.auth root@192.168.100.10:/tmp/.auth
-scp ./pipeline6/jenkins/deploy/publish root@192.168.100.10:/tmp/publish
-ssh root@192.168.100.10 "bash /tmp/publish"
+scp  /tmp/.auth root@Node1:/tmp/.auth
+scp ./pipeline6/jenkins/deploy/publish root@Node1:/tmp/publish
+ssh root@Node1 "bash /tmp/publish"
