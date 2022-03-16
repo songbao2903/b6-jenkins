5bce142e4f6cdb37f91251613faf0549782a36e6
songbao2903 <93124426+songbao2903@users.noreply.github.com>
2022-03-16 15:08:02 +0700

    Update deploy.sh

--- pipeline6/jenkins/deploy/deploy.sh
+++ pipeline6/jenkins/deploy/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo mave-project > /tmp/.auth
+echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 #scp  /tmp/.auth root@192.168.100.10:/tmp/.auth
 scp  /tmp/.auth root@192.168.100.10:/tmp/.auth
-scp publish.sh root@192.168.100.10:/tmp/publish
-ssh root@192.168.100.10 "bash /tmp/publish"
+scp ./pipeline6/jenkins/deploy/publish root@192.168.100.10:/tmp/publish
+ssh root@192.168.100.10 " /tmp/publish"
